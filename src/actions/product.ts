c39ca7e89f3abc6f9b6d7a03f945ad857d2f279d
yerald0108 <yeraldfuste@gmail.com>
2024-12-11 14:23:46 -0500

    end-search

--- src/actions/product.ts
+++ src/actions/product.ts
@@ -92,3 +92,17 @@ export const getProductBySlug = async (slug: string) => {
 
     return data;
 };
+
+export const searchProducts = async (searchTerm: string) => {
+    const {data, error} = await supabase
+        .from('products')
+        .select('*, variants(*)')
+        .ilike('name', `%${searchTerm}%`);
+
+    if (error) {
+        console.log(error.message);
+        throw new Error(error.message);
+    }
+
+    return data;
+};
--- src/components/shared/Search.tsx
+++ src/components/shared/Search.tsx
@@ -1,3 +1,83 @@
+import { useState } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
+import { useGlobalStore } from "../../store/global.store";
+import { formatPrice } from "../../helpers";
+import { searchProducts } from "../../actions";
+import { Product } from "../../interfaces";
+
 export const Search = () => {
-    return <div>Search</div>;
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResult] = useState<Product[]>([]);
+
+    const closeSheet = useGlobalStore(state => state.closeSheet);
+
+    const handleSearch = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        if(searchTerm.trim()){
+            const products = await searchProducts(searchTerm);
+            setSearchResult(products);
+            
+            
+        }
+    };
+
+    return (
+        <>
+            <div className="py-5 px-7 flex gap-10 items-center border-b border-slate-200">
+                <form
+                    className="flex gap-3 items-center flex-1"
+                    onSubmit={handleSearch}
+                >
+                    <HiOutlineSearch size={22} />
+                    <input 
+                        type="text" 
+                        placeholder="¿Qué buscas?" 
+                        className="outline-none w-full text-sm"
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                    />
+                </form>
+
+                <button onClick={closeSheet}>
+                    <IoMdClose size={22} className="text-black" />
+                </button>
+            </div>
+
+            <div className="p-5">
+                {searchResults.length > 0 ? (
+                    <ul>    
+                    {searchResults.map(product => (
+                        <li className="py-2" key={product.id}>
+                        <button className="flex items-center gap-3">
+                            <img src={product.images[0]} alt={product.name} className="h-20 w-20 object-contain p-3" />
+                            <div className="flex flex-col gap-1">
+                                <p className="text-sm font-semibold hover:underline">
+                                    {product.name}
+                                </p>
+
+                                <p className="text-[13px] text-gray-600">
+                                    {product.variants[0].storage} /{' '}
+                                    {product.variants[0].color_name}
+                                </p>
+
+                                <p className="text-sm font-medium text-gray-600">
+                                    {formatPrice(product.variants[0].price)}
+                                </p>
+                            </div>
+                        </button>
+                    </li>
+                    ))}
+                </ul>
+                ) : (
+                    <p className="text-sm text-gray-600">
+                        No se encontraron resultados
+                    </p>
+                )
+            }
+            </div>
+        </>
+    )
 };
